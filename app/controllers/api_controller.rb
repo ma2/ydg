0b63b46b66c01a39e8ad1e99c9eebb3acbad5883
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2017-06-26 22:43:23 +0900

    # デバッグ

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -28,14 +28,14 @@ class ApiController < ApplicationController
   end
 
   def event_handler(event)
-    Rails.logger.info(event.inspect)
+    Rails.logger.info(event.type)
     case event.type
     when Line::Bot::Event::MessageType::Text
       reply_to_message(event)
     when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
       reply_to_image(event)
-    # when Line::Bot::Event::Postback
-    #   reply_to_poctback(event)
+      # when Line::Bot::Event::Postback
+      #   reply_to_poctback(event)
     end
 
   end
@@ -45,36 +45,36 @@ class ApiController < ApplicationController
     msg = event.message['text']
     if msg == 'クイズ'
       message = {
-          type: 'template',
-          altText: '漢字のクイズ',
-          template: {
-              type: 'buttons',
-              title: '匕首',
-              text: 'なんて読むかな？',
-              actions: [
-                  {
-                      type: 'postback',
-                      label: 'いくび',
-                      data: 'q=1&choice=1'
-                  },
-                  {
-                      type: 'postback',
-                      label: 'あいくち',
-                      data: 'q=1&choice=2'
-                  },
-                  {
-                      type: 'uri',
-                      label: 'おしゅ',
-                      data: 'q=1&choice=3'
-                  }
-              ]
-          }
+        type: 'template',
+        altText: '漢字のクイズ',
+        template: {
+          type: 'buttons',
+          title: '匕首',
+          text: 'なんて読むかな？',
+          actions: [
+            {
+              type: 'postback',
+              label: 'いくび',
+              data: 'q=1&choice=1'
+            },
+            {
+              type: 'postback',
+              label: 'あいくち',
+              data: 'q=1&choice=2'
+            },
+            {
+              type: 'postback',
+              label: 'おしゅ',
+              data: 'q=1&choice=3'
+            }
+          ]
+        }
       }
     else
       # オウム返し
       message = {
-          type: 'text',
-          text: event.message['text']
+        type: 'text',
+        text: event.message['text']
       }
     end
     @client.reply_message(event['replyToken'], message)
@@ -83,16 +83,16 @@ class ApiController < ApplicationController
   # ポストバック（ユーザの選択）に返事
   def reply_to_postback(event)
     message = {
-        type: 'text',
-        text: 'そうかもねー'
+      type: 'text',
+      text: 'そうかもねー'
     }
     @client.reply_message(event['replyToken'], message)
   end
 
   def reply_to_image(event)
     message = {
-        type: 'text',
-        text: '画像だよねー'
+      type: 'text',
+      text: '画像だよねー'
     }
     @client.reply_message(event['replyToken'], message)
   end
