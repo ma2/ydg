65dafa6a052179c1de177c2ed831e2ce4c0e4f1a
keiichi matsunaga <keiichi.2004.matsunaga@gmail.com>
2017-06-26 22:03:48 +0900

    # event_handlerを呼び出してなかった

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -14,21 +14,7 @@ class ApiController < ApplicationController
 
     events = @client.parse_events_from(body)
     events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-              type: 'text',
-              text: event.message['text']
-          }
-          @client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = @client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
+      event_handler(event)
     end
     render text: 'OK'
   end
